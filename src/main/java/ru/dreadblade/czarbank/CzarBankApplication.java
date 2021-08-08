104d0dd935b753ff139aa538d946165dd53bde2a
Dreadblade- <sqandex1suggestions@gmail.com>
2021-08-08 22:33:39 +0200

    Initial commit

---
+++ src/main/java/ru/dreadblade/czarbank/CzarBankApplication.java
@@ -0,0 +1,13 @@
+package ru.dreadblade.czarbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CzarBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CzarBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/dreadblade/czarbank/CzarBankApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dreadblade.czarbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CzarBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
