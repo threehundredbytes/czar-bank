750daee86d0df7a0f02781c21be07fac26e9b04e
Dreadblade- <sqandex1suggestions@gmail.com>
2021-09-13 10:48:07 +0200

    Added currency exchange

--- src/test/java/ru/dreadblade/czarbank/api/controller/CurrencyIntegrationTest.java
+++ src/test/java/ru/dreadblade/czarbank/api/controller/CurrencyIntegrationTest.java
@@ -13,11 +13,18 @@ import org.springframework.test.context.jdbc.Sql;
 import ru.dreadblade.czarbank.api.mapper.CurrencyMapper;
 import ru.dreadblade.czarbank.api.model.request.CurrencyRequestDTO;
 import ru.dreadblade.czarbank.api.model.response.CurrencyResponseDTO;
+import ru.dreadblade.czarbank.domain.Currency;
 import ru.dreadblade.czarbank.exception.ExceptionMessage;
 import ru.dreadblade.czarbank.repository.BankAccountRepository;
 import ru.dreadblade.czarbank.repository.CurrencyRepository;
 import ru.dreadblade.czarbank.repository.ExchangeRateRepository;
+import ru.dreadblade.czarbank.repository.security.UserRepository;
+import ru.dreadblade.czarbank.service.BankAccountService;
+import ru.dreadblade.czarbank.service.CurrencyService;
+import ru.dreadblade.czarbank.service.ExchangeRateService;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -35,16 +42,28 @@ public class CurrencyIntegrationTest extends BaseIntegrationTest {
     public static final String CURRENCIES_API_URL = "/api/currencies";
 
     @Autowired
+    BankAccountService bankAccountService;
+
+    @Autowired
     BankAccountRepository bankAccountRepository;
 
     @Autowired
+    ExchangeRateService exchangeRateService;
+
+    @Autowired
     ExchangeRateRepository exchangeRateRepository;
+
+    @Autowired
+    CurrencyService currencyService;
 
     @Autowired
     CurrencyRepository currencyRepository;
 
     @Autowired
     CurrencyMapper currencyMapper;
+
+    @Autowired
+    UserRepository userRepository;
 
     @Nested
     @DisplayName("findAll() Tests")
@@ -166,6 +185,191 @@ public class CurrencyIntegrationTest extends BaseIntegrationTest {
                     .andExpect(status().isBadRequest())
                     .andExpect(jsonPath("$.message")
                             .value(ExceptionMessage.UNSUPPORTED_CURRENCY.getMessage()));
+        }
+    }
+
+    @Nested
+    @DisplayName("exchangeCurrency() Tests")
+    class exchangeCurrencyTests {
+        @Test
+        void exchangeCurrency_fromRubToUsd_isSuccessful() {
+            Currency sourceCurrency = currencyRepository.findByCode("RUB").orElseThrow();
+            Currency targetCurrency = currencyRepository.findByCode("USD").orElseThrow();
+
+            Assertions.assertThat(sourceCurrency.getCode()).isNotEqualTo(targetCurrency.getCode());
+
+            BigDecimal exchangeRate = exchangeRateService.findAllLatest().stream()
+                    .filter(rate -> rate.getCurrency().getCode().equals(targetCurrency.getCode()))
+                    .findFirst().orElseThrow()
+                    .getExchangeRate();
+
+            BigDecimal amountInRub = new BigDecimal(10000L);
+
+            BigDecimal amountInUsd = currencyService.exchangeCurrency(sourceCurrency, amountInRub, targetCurrency);
+
+            Assertions.assertThat(amountInUsd).isEqualByComparingTo(amountInRub.divide(exchangeRate, RoundingMode.HALF_EVEN));
+        }
+
+        @Test
+        void exchangeCurrency_fromUsdToRub_isSuccessful() {
+            Currency sourceCurrency = currencyRepository.findByCode("USD").orElseThrow();
+            Currency targetCurrency = currencyRepository.findByCode("RUB").orElseThrow();
+
+            Assertions.assertThat(sourceCurrency.getCode()).isNotEqualTo(targetCurrency.getCode());
+
+            BigDecimal exchangeRate = exchangeRateService.findAllLatest().stream()
+                    .filter(rate -> rate.getCurrency().getCode().equals(sourceCurrency.getCode()))
+                    .findFirst().orElseThrow()
+                    .getExchangeRate();
+
+            BigDecimal amountInUsd = new BigDecimal(100L);
+
+            BigDecimal amountInRub = currencyService.exchangeCurrency(sourceCurrency, amountInUsd, targetCurrency);
+
+            Assertions.assertThat(amountInRub).isEqualByComparingTo(amountInUsd.multiply(exchangeRate));
+        }
+
+        @Test
+        void exchangeCurrency_fromRubToJpy_isSuccessful() {
+            Currency sourceCurrency = currencyRepository.findByCode("RUB").orElseThrow();
+            Currency targetCurrency = currencyRepository.findByCode("JPY").orElseThrow();
+
+            Assertions.assertThat(sourceCurrency.getCode()).isNotEqualTo(targetCurrency.getCode());
+
+            BigDecimal exchangeRate = exchangeRateService.findAllLatest().stream()
+                    .filter(rate -> rate.getCurrency().getCode().equals(targetCurrency.getCode()))
+                    .findFirst().orElseThrow()
+                    .getExchangeRate();
+
+            BigDecimal amountInRub = new BigDecimal(10000L);
+            BigDecimal amountInJpy = currencyService.exchangeCurrency(sourceCurrency, amountInRub, targetCurrency);
+
+            Assertions.assertThat(amountInJpy).isEqualByComparingTo(amountInRub.divide(exchangeRate, RoundingMode.HALF_EVEN));
+        }
+
+        @Test
+        void exchangeCurrency_fromJpyToRub_isSuccessful() {
+            Currency sourceCurrency = currencyRepository.findByCode("JPY").orElseThrow();
+            Currency targetCurrency = currencyRepository.findByCode("RUB").orElseThrow();
+
+            Assertions.assertThat(sourceCurrency.getCode()).isNotEqualTo(targetCurrency.getCode());
+
+            BigDecimal exchangeRate = exchangeRateService.findAllLatest().stream()
+                    .filter(rate -> rate.getCurrency().getCode().equals(sourceCurrency.getCode()))
+                    .findFirst().orElseThrow()
+                    .getExchangeRate();
+
+            BigDecimal amountInJpy = new BigDecimal(10000L);
+
+            BigDecimal amountInRub = currencyService.exchangeCurrency(sourceCurrency, amountInJpy, targetCurrency);
+
+            Assertions.assertThat(amountInRub).isEqualByComparingTo(amountInJpy.multiply(exchangeRate));
+        }
+
+        @Test
+        void exchangeCurrency_fromRubToRub_isSuccessful() {
+            Currency sourceCurrency = currencyRepository.findByCode("RUB").orElseThrow();
+            Currency targetCurrency = currencyRepository.findByCode("RUB").orElseThrow();
+
+            Assertions.assertThat(sourceCurrency.getCode()).isEqualTo(targetCurrency.getCode());
+
+            BigDecimal amountInRub = new BigDecimal(10000L);
+
+            BigDecimal exchangedAmountInRub = currencyService.exchangeCurrency(sourceCurrency, amountInRub, targetCurrency);
+
+            Assertions.assertThat(amountInRub).isEqualByComparingTo(exchangedAmountInRub);
+        }
+
+        @Test
+        void exchangeCurrency_fromUsdToUsd_isSuccessful() {
+            Currency sourceCurrency = currencyRepository.findByCode("USD").orElseThrow();
+            Currency targetCurrency = currencyRepository.findByCode("USD").orElseThrow();
+
+            Assertions.assertThat(sourceCurrency.getCode()).isEqualTo(targetCurrency.getCode());
+
+            BigDecimal amountInUsd = new BigDecimal(10000L);
+
+            BigDecimal exchangedAmountInUsd = currencyService.exchangeCurrency(sourceCurrency, amountInUsd, targetCurrency);
+
+            Assertions.assertThat(amountInUsd).isEqualByComparingTo(exchangedAmountInUsd);
+        }
+
+        @Test
+        void exchangeCurrency_fromEurToUsd_isSuccessful() {
+            Currency sourceCurrency = currencyRepository.findByCode("EUR").orElseThrow();
+            Currency targetCurrency = currencyRepository.findByCode("USD").orElseThrow();
+
+            Assertions.assertThat(sourceCurrency.getCode()).isNotEqualTo(targetCurrency.getCode());
+
+            BigDecimal exchangeRateToRub = exchangeRateService.findAllLatest().stream()
+                    .filter(rate -> rate.getCurrency().getCode().equals(sourceCurrency.getCode()))
+                    .findFirst().orElseThrow()
+                    .getExchangeRate();
+
+            BigDecimal exchangeRateFromRub = exchangeRateService.findAllLatest().stream()
+                    .filter(rate -> rate.getCurrency().getCode().equals(targetCurrency.getCode()))
+                    .findFirst().orElseThrow()
+                    .getExchangeRate();
+
+            BigDecimal amountInEur = new BigDecimal(10000L);
+
+            BigDecimal amountInUsd = currencyService.exchangeCurrency(sourceCurrency, amountInEur, targetCurrency);
+
+            BigDecimal expected = amountInEur.multiply(exchangeRateToRub).divide(exchangeRateFromRub, RoundingMode.HALF_EVEN);
+
+            Assertions.assertThat(amountInUsd).isEqualByComparingTo(expected);
+        }
+
+        @Test
+        void exchangeCurrency_fromJpyToUsd_isSuccessful() {
+            Currency sourceCurrency = currencyRepository.findByCode("JPY").orElseThrow();
+            Currency targetCurrency = currencyRepository.findByCode("USD").orElseThrow();
+
+            Assertions.assertThat(sourceCurrency.getCode()).isNotEqualTo(targetCurrency.getCode());
+
+            BigDecimal exchangeRateToRub = exchangeRateService.findAllLatest().stream()
+                    .filter(rate -> rate.getCurrency().getCode().equals(sourceCurrency.getCode()))
+                    .findFirst().orElseThrow()
+                    .getExchangeRate();
+
+            BigDecimal exchangeRateFromRub = exchangeRateService.findAllLatest().stream()
+                    .filter(rate -> rate.getCurrency().getCode().equals(targetCurrency.getCode()))
+                    .findFirst().orElseThrow()
+                    .getExchangeRate();
+
+            BigDecimal amountInJpy = new BigDecimal(10000L);
+
+            BigDecimal amountInUsd = currencyService.exchangeCurrency(sourceCurrency, amountInJpy, targetCurrency);
+
+            BigDecimal expected = amountInJpy.multiply(exchangeRateToRub).divide(exchangeRateFromRub, RoundingMode.HALF_EVEN);
+
+            Assertions.assertThat(amountInUsd).isEqualByComparingTo(expected);
+        }
+
+        @Test
+        void exchangeCurrency_fromUsdToJpy_isSuccessful() {
+            Currency sourceCurrency = currencyRepository.findByCode("USD").orElseThrow();
+            Currency targetCurrency = currencyRepository.findByCode("JPY").orElseThrow();
+
+            Assertions.assertThat(sourceCurrency.getCode()).isNotEqualTo(targetCurrency.getCode());
+
+            BigDecimal exchangeRateToRub = exchangeRateService.findAllLatest().stream()
+                    .filter(rate -> rate.getCurrency().getCode().equals(sourceCurrency.getCode()))
+                    .findFirst().orElseThrow()
+                    .getExchangeRate();
+
+            BigDecimal exchangeRateFromRub = exchangeRateService.findAllLatest().stream()
+                    .filter(rate -> rate.getCurrency().getCode().equals(targetCurrency.getCode()))
+                    .findFirst().orElseThrow()
+                    .getExchangeRate();
+
+            BigDecimal amountInUsd = new BigDecimal(10000L);
+
+            BigDecimal amountInJpy = currencyService.exchangeCurrency(sourceCurrency, amountInUsd, targetCurrency);
+
+            BigDecimal expected = amountInUsd.multiply(exchangeRateToRub).divide(exchangeRateFromRub, RoundingMode.HALF_EVEN);
+
+            Assertions.assertThat(amountInJpy).isEqualByComparingTo(expected);
         }
     }
 }
--- src/test/resources/bank-account/bank-accounts-insertion.sql
+++ src/test/resources/bank-account/bank-accounts-insertion.sql
@@ -9,26 +9,35 @@ insert into bank_account_type (id, name, transaction_commission) values (23, 'Ju
 insert into bank_account_type (id, name, transaction_commission) values (24, 'Standard', 0.04);
 insert into bank_account_type (id, name, transaction_commission) values (25, 'Unused account type', 0.10);
 
-insert into currency (id, code, symbol) values (26, 'RUB', '₽');
-insert into currency (id, code, symbol) values (27, 'USD', '$');
-insert into currency (id, code, symbol) values (28, 'EUR', '€');
+insert into currency (id, code, symbol)
+values (26, 'RUB', '₽'),
+       (27, 'USD', '$'),
+       (28, 'EUR', '€'),
+       (29, 'JPY', '¥');
 
 insert into exchange_rate (id, date, exchange_rate, currency_id)
-values (29, '2021-09-01', 73.28, 27),
-       (30, '2021-09-01', 86.67, 28),
-       (31, '2021-09-02', 73.19, 27),
-       (32, '2021-09-02', 86.39, 28),
+values (31, '2021-09-01', 73.28, 27),
+       (32, '2021-09-02', 73.19, 27),
        (33, '2021-09-03', 72.85, 27),
-       (34, '2021-09-03', 86.30, 28),
-       (35, '2021-09-04', 72.85, 27),
-       (36, '2021-09-04', 86.54, 28),
-       (37, '2021-09-05', 72.85, 27),
-       (38, '2021-09-05', 86.54, 28);
+       (34, '2021-09-04', 72.85, 27),
+       (35, '2021-09-05', 72.85, 27),
 
-insert into bank_account (id, balance, is_closed, number, owner_id, used_currency_id, bank_account_type_id) values (39, 15000, false, '39903336089073190794', 19, 26, 21);
-insert into bank_account (id, balance, is_closed, number, owner_id, used_currency_id, bank_account_type_id) values (40, 5000, false, '33390474811219980161', 20, 26, 22);
-insert into bank_account (id, balance, is_closed, number, owner_id, used_currency_id, bank_account_type_id) values (41, 2000, false, '38040432731497506063', 18, 26, 23);
-insert into bank_account (id, balance, is_closed, number, owner_id, used_currency_id, bank_account_type_id) values (42, 500, false, '36264421013439107929', 20, 26, 24);
-insert into bank_account (id, balance, is_closed, number, owner_id, used_currency_id, bank_account_type_id) values (43, 1500, false, '32541935657215432384', 19, 26, 24);
+       (36, '2021-09-01', 86.67, 28),
+       (37, '2021-09-02', 86.39, 28),
+       (38, '2021-09-03', 86.30, 28),
+       (39, '2021-09-04', 86.54, 28),
+       (40, '2021-09-05', 86.54, 28),
 
-alter sequence hibernate_sequence restart 44;
+       (41, '2021-09-01', 0.67, 29),
+       (42, '2021-09-02', 0.67, 29),
+       (43, '2021-09-03', 0.67, 29),
+       (44, '2021-09-04', 0.67, 29),
+       (45, '2021-09-05', 0.67, 29);
+
+insert into bank_account (id, balance, is_closed, number, owner_id, used_currency_id, bank_account_type_id) values (46, 15000, false, '39903336089073190794', 19, 26, 21);
+insert into bank_account (id, balance, is_closed, number, owner_id, used_currency_id, bank_account_type_id) values (47, 5000, false, '33390474811219980161', 20, 26, 22);
+insert into bank_account (id, balance, is_closed, number, owner_id, used_currency_id, bank_account_type_id) values (48, 2000, false, '38040432731497506063', 18, 26, 23);
+insert into bank_account (id, balance, is_closed, number, owner_id, used_currency_id, bank_account_type_id) values (49, 500, false, '36264421013439107929', 20, 26, 24);
+insert into bank_account (id, balance, is_closed, number, owner_id, used_currency_id, bank_account_type_id) values (50, 1500, false, '32541935657215432384', 19, 26, 24);
+
+alter sequence hibernate_sequence restart 51;
