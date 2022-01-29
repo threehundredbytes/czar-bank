52cfda3db8f7eede87fab67b456b312133e2c1e0
Dreadblade- <sqandex1suggestions@gmail.com>
2022-01-29 20:10:26 +0200

    Changed @Length to @Size

--- src/main/java/ru/dreadblade/czarbank/api/model/request/BankAccountTypeRequestDTO.java
+++ src/main/java/ru/dreadblade/czarbank/api/model/request/BankAccountTypeRequestDTO.java
@@ -1,13 +1,13 @@
 package ru.dreadblade.czarbank.api.model.request;
 
 import lombok.*;
-import org.hibernate.validator.constraints.Length;
 import ru.dreadblade.czarbank.api.model.request.validation.CreateRequest;
 import ru.dreadblade.czarbank.api.model.request.validation.UpdateRequest;
 
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.math.BigDecimal;
 
 @Getter
@@ -17,7 +17,7 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 public class BankAccountTypeRequestDTO {
     @NotBlank(message = "Bank account type name must be not empty", groups = CreateRequest.class)
-    @Length(message = "The maximum length of the bank account type name is 32 characters",
+    @Size(message = "The maximum length of the bank account type name is 32 characters",
             groups = { CreateRequest.class, UpdateRequest.class }, max = 32)
     private String name;
 
